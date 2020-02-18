4851288910e7a02b43365886dbfb63042dca7db2
Jasonmart1002 <Jason.mart1002@gmail.com>
2020-02-18 22:16:18 +0000

    working

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -15,7 +15,7 @@ const starWars = {
 	marginTop: "5px",
 	display: "flex",
 	justifyContent: "center",
-	height: "300px",
+	height: "450px",
 	perspective: "800px",
 	color: "#feda4a",
 	fontFamily: "'Pathway Gothic One', sans-serif",
@@ -37,24 +37,15 @@ const crawl = {
 export function Jumbotron() {
 	return (
 		<div className="container">
-			<audio preload="auto" autoPlay>
-				<source
-					src="src/music/Star_Wars_original_opening_crawl_1977.mp3"
-					type="audio/mpeg"
-				/>
-			</audio>
 			<header className="jumbotron my-4" style={jumboStyle}>
 				<section className="star-wars" style={starWars}>
 					<div className="crawl" style={crawl}>
 						<div className="title">
 							<br />
 							<br />
-							<br />
-							<br />
-							<br />
 							<img src="https://lh3.googleusercontent.com/proxy/B0uUgcRimAFesSeqmVghxtLlbavurni_rPP33uQfCBK72FWzHtSe-_ws7G9F2d3wT78DacCfJYfZ4nwzhAGa6syPzpmD3mvHkWqGzKl-GGlOxUIjDA7qAHE" />
 						</div>
-						{/* Episode 4 */}
+						{/* Episode IV */}
 						<p>
 							<br />
 							<br />
@@ -112,6 +103,7 @@ export function Jumbotron() {
 							<br />
 						</p>
 
+						{/* Episode VI */}
 						<p>Episode VI</p>
 						<h1>THE RETURN OF THE JEDI</h1>
 
@@ -131,78 +123,6 @@ export function Jumbotron() {
 							When completed, this ultimate weapon will spell
 							certain doom for the small band of rebels struggling
 							to restore freedom to the galaxy…
-							<br />
-							<br />
-							<br />
-						</p>
-
-						<p>Episode I</p>
-						<h1>THE PHANTOM MENACE</h1>
-
-						<p>
-							Turmoil has engulfed the Galactic Republic. The
-							taxation of trade routes to outlying star systems is
-							in dispute.
-						</p>
-						<p>
-							Hoping to resolve the matter with a blockade of
-							deadly battleships, the greedy Trade Federation has
-							stopped all shipping to the small planet of Naboo.
-						</p>
-						<p>
-							While the congress of the Republic endlessly debates
-							this alarming chain of events, the Supreme
-							Chancellor has secretly dispatched two Jedi Knights,
-							the guardians of peace and justice in the galaxy, to
-							settle the conflict….
-							<br />
-							<br />
-							<br />
-						</p>
-
-						<p>Episode II</p>
-						<h1>ATTACK OF THE CLONES</h1>
-
-						<p>
-							There is unrest in the Galactic Senate. Several
-							thousand solar systems have declared their
-							intentions to leave the Republic.
-						</p>
-						<p>
-							This separatist movement, under the leadership of
-							the mysterious Count Dooku, has made it difficult
-							for the limited number of Jedi Knights to maintain
-							peace and order in the galaxy.
-						</p>
-						<p>
-							Senator Amidala, the former Queen of Naboo, is
-							returning to the Galactic Senate to vote on the
-							critical issue of creating an ARMY OF THE REPUBLIC
-							to assist the overwhelmed Jedi....
-							<br />
-							<br />
-							<br />
-						</p>
-
-						<p>Episode III</p>
-						<h1>REVENGE OF THE SITH</h1>
-
-						<p>
-							War! The Republic is crumbling under attacks by the
-							ruthless Sith Lord, Count Dooku. There are heroes on
-							both sides. Evil is everywhere.
-						</p>
-						<p>
-							In a stunning move, the fiendish droid leader,
-							General Grievous, has swept into the Republic
-							capital and kidnapped Chancellor Palpatine, leader
-							of the Galactic Senate. As the Separatist Droid Army
-							attempts to flee the besieged capital with their
-							valuable hostage, two Jedi Knights lead a desperate
-							mission to rescue the captive Chancellor....
-							<br />
-							<br />
-							<br />
 						</p>
 					</div>
 				</section>
